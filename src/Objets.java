341bbfb6e04ed5f4bc53eb3bf3a3a38ad8eaee39
Lionel Viguier <lvigui03@pcalb-mm0000.univ-jfc.fr>
2019-03-19 11:36:10 +0100

    première version

---
+++ src/Objets.java
@@ -0,0 +1,14 @@
+
+public class Objets {
+	int ID;
+	int nombre;
+	boolean nul;
+	
+	public Objets(int id, int nombre, boolean vrais) {
+		this.ID = id;
+		this.nombre = nombre;
+		this.nul = vrais;
+		
+	}
+
+}
